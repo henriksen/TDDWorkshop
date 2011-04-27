548f9dba2f85450aeef327b36e5a7b1fce5e9e6e
Glenn F. Henriksen <glenn@henriksen.no>
2011-04-27 19:29:56 +0200

    OrderProcessor: First test

--- 02/OrderProcessor/OrderProcessor/OrderProcessor.cs
+++ 02/OrderProcessor/OrderProcessor/OrderProcessor.cs
@@ -4,6 +4,13 @@ namespace OrderProcessorSample
 {
     public class OrderProcessor
     {
+        private readonly INotify _notify;
+
+        public OrderProcessor(INotify notify)
+        {
+            _notify = notify;
+        }
+
         public void Ship(Order order)
         {
         }
@@ -13,11 +20,11 @@ namespace OrderProcessorSample
 /*
     Given an order where the customer wants notification of shipment
     When shipping that order
-    The Notification service should be called
+    Then the Notification service should be called
     and given that order
 
     Given an order where the customer does not  want notification of shipment
     When shipping that order
-    The Notification service should not be called
+    Then the Notification service should not be called
 */
 
---
+++ 02/OrderProcessor/OrderProcessor/OrderProcessorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace OrderProcessorSample
+{
+    [TestFixture]
+    class OrderProcessorTests
+    {
+        [Test]
+        public void CustomerWantsNotification_NotificationShouldBeCalled()
+        {
+            var order = new Order();
+            order.NotifyCustomerOfShipment = true;
+            var mockNotifier = new Moq.Mock<INotify>();
+            var orderProcessor =
+                new OrderProcessor(mockNotifier.Object);
+
+            orderProcessor.Ship(order);
+
+            mockNotifier.Verify(notifier => 
+                notifier.Notify(order));
+        }
+    }
+}
